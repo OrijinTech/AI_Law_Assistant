1626c5e5b00d40de573718d737b5bfc7ea2d9ab3
Yongxiang <yongxiangj1999@gmail.com>
2021-09-30 00:13:17 -0400

    Added inline documentations for functions.

--- Word_splitter.py
+++ Word_splitter.py
@@ -7,11 +7,25 @@ import jieba  # 结巴分词 工具
 
 # 初始化结巴分词
 def setup_jieba():
+    '''
+    初始化分词工具
+    :return: 无
+    :rtype: 无
+    '''
     jieba.initialize()
 
 #输入：用户信息，分词模式 (True = 全模式)
 #输出：词组的list
 
 def fen_ci(message, cut_mode):
+    '''
+    分词道具
+    :param message: 用户输入的信息。
+    :type message: str
+    :param cut_mode: 分词模式，True为全模式
+    :type cut_mode: boolean
+    :return: 分好词的list
+    :rtype: list
+    '''
     seg_list = jieba.cut(message, cut_mode)
     return list(seg_list)
