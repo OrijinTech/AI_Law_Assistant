c6b3370f0a112efdbb4af6137dbeb467b25d8be5
Yongxiang <yongxiangj1999@gmail.com>
2021-11-17 11:55:06 -0500

    AI Chatbot modification

--- AI_Assistant/AI_bot.py
+++ AI_Assistant/AI_bot.py
@@ -5,14 +5,16 @@ import json
 import nltk
 import numpy
 import tflearn
-from nltk.stem.lancaster import LancasterStemmer
+from nltk.stem.lancaster import LancasterStemmer #Used to analyze words from the sentence (getting the root of the word --> only for English)
 import pickle
 
 stemmer = LancasterStemmer()
 
-
+#Open the json file with the train data
 with open("../Language_Data/intents.json") as file:
+    #returning the Json object
     data = json.load(file)
+    #print(data)
 
 try:
     with open("data.pickle", "rb") as f:
@@ -20,22 +22,23 @@ try:
 except:
     words = []
     labels = []
-    docs = []
     # Each pattern should be tagged with a tag stored in docs_y
-    docs_x = []
-    docs_y = []
+    docs_x = [] #stores the tokenized word lists
+    docs_y = [] #stores the tag for each tokenized word list
 
+    #Iterate through the "intents" in the json file.
     for intent in data["intents"]:
+        #For each pattern inside each intent, we want to tokenize the words that are in sentences.
         for pattern in intent["patterns"]:
-            wrds = nltk.word_tokenize(pattern)
-            words.extend(wrds)
-            docs_x.append(wrds)
+            tokenized_list_of_words = nltk.word_tokenize(pattern) #This must be changed to the Jieba version of tokenizer.
+            words.extend(tokenized_list_of_words) #add the tokenized words into the "words" list
+            docs_x.append(tokenized_list_of_words)
             docs_y.append(intent["tag"])
             if intent["tag"] not in labels:
                 labels.append(intent["tag"])
 
-    words = [stemmer.stem(w.lower()) for w in words if w not in "?"]
-    words = sorted(list(set(words)))
+    words = [stemmer.stem(w.lower()) for w in words if w not in "?"] #convert words into lower case
+    words = sorted(list(set(words))) #sort and remove duplicates
     labels = sorted(labels)
 
     training = []
