6fe1c71512f38af419adfa9e4ee6b639751bf572
Yongxiang <yongxiangj1999@gmail.com>
2022-01-17 16:33:02 -0500

    Added test bot.

--- AI_Assistant/testbot_En.py
+++ AI_Assistant/testbot_En.py
@@ -18,9 +18,10 @@ initial_state = AI_StateMachine.States.CHAT
 
 stemmer = LancasterStemmer()
 # Open the json file with the train data
-with open("../Language_Data/Law_Data.json", encoding="utf8") as file:
+with open("../Language_Data/intents.json", encoding="utf8") as file:
     # returning the Json object
     data = json.load(file)
+
 
 # Comment this out after updating Law_Data json file
 # try:
@@ -35,16 +36,15 @@ docs_x = []  # stores the tokenized word lists
 docs_y = []  # stores the law_type for each tokenized word list
 word_docs_x = []
 # Iterate through the "intents" in the json file.
-for intent in data["law_database"]:
-    # For each pattern inside each intent, we want to tokenize the words that are in sentences.
-    for pattern in intent["law_keywords"]:
-        tokenized_list_of_words = jieba.cut(pattern, cut_all=False)  # This is the tokenizer for the Chinese language.
-        toklist = list(tokenized_list_of_words)
-        words.extend(toklist)  # add the tokenized words into the "words" list
-        docs_x.append(toklist)
-        docs_y.append(intent["law_type"])
-        if intent["law_type"] not in labels:
-            labels.append(intent["law_type"])
+for intent in data["intents"]:
+    for pattern in intent["patterns"]:
+        tokenized_list_of_words = nltk.word_tokenize(pattern)
+        words.extend(tokenized_list_of_words)
+        docs_x.append(tokenized_list_of_words)
+        print(tokenized_list_of_words)
+        docs_y.append(intent["tag"])
+        if intent["tag"] not in labels:
+            labels.append(intent["tag"])
 
 words = [stemmer.stem(w.lower()) for w in words if w not in "?"]  # convert words into lower case
 # words = sorted(list(set(words)))  # sort and remove duplicates
@@ -101,39 +101,31 @@ model.fit(training, output, n_epoch=epoch_num, batch_size=batchsize,
 #     model.save("model.tflearn")
 
 
+def bag_of_words(s, words):
+    bag = [0 for _ in range(len(words))]
+    s_words = nltk.word_tokenize(s)
+    s_words = [stemmer.stem(word.lower()) for word in s_words]
 
-def bag_of_words_chinese(s, words):
-    # SETUP for Chinese sentence processing
-    stop_words = set(stopwords(["zh"]))
-    bag_chinese = [0 for _ in range(len(words))]
-    s_words = jieba.cut(s, cut_all=False)
-
-    # stopwords filtration
-    s_words_filtered = []
-    for w in s_words:
-        if w not in stop_words:
-            s_words_filtered.append(w)
-    # unicode_s_words_filtered = [i.decode('utf-8') for i in s_words_filtered]
-    for se in s_words_filtered:
+    for se in s_words:
         for i, w in enumerate(words):
             if w == se:
-                bag_chinese[i] = 1
-    return np.array(bag_chinese)
+                bag[i] = 1
+    return np.array(bag)
 
 
-def chat():
-    support_fnc.clear_chat()
+def chat_english():
     global initial_state
+
     while True:
         # Chat State
         if initial_state == AI_StateMachine.States.CHAT:
-            print("您好, 我是普法小助手YU, 有什么可以帮助您的吗？（输入 quit 来结束对话）")
+            print("请开始说话（输入 quit 来结束对话）")
             while True:
                 inp = input("您： ")
                 if inp.lower() == "quit":
                     initial_state = AI_StateMachine.States.QUIT
                     break
-                results = model.predict([bag_of_words_chinese(inp, words)])[0]  # Chinese Version
+                results = model.predict([bag_of_words(inp, words)])[0]
                 results_index = numpy.argmax(results)
                 law_type = labels[results_index]
                 result_percentage = round(results[results_index] * 100, 2)
@@ -155,22 +147,22 @@ def chat():
                 if law_type == "学习模式":
                     initial_state = AI_StateMachine.States.LEARN
                     break
-        # Learn State
-        if initial_state == AI_StateMachine.States.LEARN:
-            while True:
-                learn_pattern = input("请输入您要我学习的文献：")
-                print("这是哪种法律种类？")
-                learn_type = input("您： ")
-                if learn_type in docs_y:
-                    data_modification.add_pattern(learn_pattern, learn_type)
-                keep_learn = input("还有其他要我学习的吗？(Y/N)： ")
-                if keep_learn == "N":
-                    initial_state = AI_StateMachine.States.CHAT
-                    break
-        # Quit the program
-        if initial_state == AI_StateMachine.States.QUIT:
-            break
+            # Learn State
+            if initial_state == AI_StateMachine.States.LEARN:
+                while True:
+                    learn_pattern = input("请输入您要我学习的文献：")
+                    print("这是哪种法律种类？")
+                    learn_type = input("您： ")
+                    if learn_type in docs_y:
+                        data_modification.add_pattern(learn_pattern, learn_type)
+                    keep_learn = input("还有其他要我学习的吗？(Y/N)： ")
+                    if keep_learn == "N":
+                        initial_state = AI_StateMachine.States.CHAT
+                        break
+            # Quit the program
+            if initial_state == AI_StateMachine.States.QUIT:
+                break
 
-# Starting Point
-chat()
 
+# STARTING POINT
+chat_english()
