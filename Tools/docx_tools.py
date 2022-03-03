e2754169ae5f75c727260d82b827d12c2bc5bf34
Yongxiang <yongxiangj1999@gmail.com>
2022-03-03 16:06:39 -0500

    UPDATE
    Added Functions:
    1.format_para
    2.delete_from_par
    3.delete_from_list
    4.updated_list_of_para
    
    Deleted Functions:
    
    Updated Functions:
    1.extract_paragraph
    
    File Added:
    
    Other:

--- Tools/docx_tools.py
+++ Tools/docx_tools.py
@@ -1,4 +1,8 @@
 import docx
+import support_fnc
+import unicodedata
+
+list_of_words_del = ["\u3000"]
 
 
 def extract_paragraph(filename):
@@ -6,12 +10,54 @@ def extract_paragraph(filename):
     par_list = []
     for para in doc.paragraphs:
         par_list.append(para.text)
-    print(par_list)
     return par_list
-# extract_paragraph("../Language_Data/docx_data/中华人民共和国义务教育法(2018修正)(FBM-CLI-1-328270).docx")
-
-def delete_paragraph(par_list):
 
 
-def delete_word(paragraph):
+def format_para(par_list, list_of_del_word):
+    unicode_list = [unicodedata.normalize('NFKC', line) for line in par_list]
+    updated_list = []
+    for par in unicode_list:
+        updated_list.append(par.strip())
+    print(updated_list)
+    for par2 in updated_list:
+        if par2 == '':
+            updated_list.remove(par2)
+        for w in list_of_words_del:
+            if w in par2:
+                par2.replace(w, '')
+    return updated_list
 
+
+def add_to_(par_list):
+    return None
+
+
+def delete_from_par(word, paragraph):
+    set_par = paragraph
+    set_par.replace(word, "")
+    return set_par
+
+
+def delete_from_list(word, par_list):
+    ret_list = par_list
+    for par in ret_list:
+        if word in par:
+            ret_list.remove(par)
+        # elif len(par) < 10:
+        #     ret_list.remove(par)
+    return ret_list
+
+
+def updated_list_of_para():
+    l1 = extract_paragraph("../Language_Data/docx_data/中华人民共和国义务教育法(2018修正)(FBM-CLI-1-328270).docx")
+    print(l1)
+    l1 = format_para(l1, list_of_words_del)
+    print(l1)
+
+
+# updated_list_of_para()
+
+txy = '  第一章 总  则\n  第二章 学  生\n  第三章 学  校\n  第四章 教  师\n  第五章 教育教学\n '
+print(txy)
+txy.replace("\n", '')
+print(txy)
